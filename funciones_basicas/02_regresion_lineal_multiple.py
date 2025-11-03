4f55b265df59c06e9fb52441d241ae70e52ba288
Maturus_Lux <43556620+MaturusLux@users.noreply.github.com>
2025-11-02 21:02:19 -0300

    Update 02_regresion_lineal_multiple.py
    Función:
    Regresión Lineal Multiple: Relación entre cantidad de hectáreas estándar, Horas de Labranza, Productos aplicados.
    Lenguaje: Python 3.7.
    Librerias: numpy, matplotlib, sklearn.

--- funciones_basicas/02_regresion_lineal_multiple.py
+++ funciones_basicas/02_regresion_lineal_multiple.py
@@ -1,4 +1,4 @@
-# 01 Regresion Lineal Multiple - Precio Hectáreas de Lote
+# 02 Regresión Lineal Múltiple - Precio Hectáreas de Lote
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
