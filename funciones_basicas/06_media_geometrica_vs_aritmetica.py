088ceb764167d55c4693c374ce629f83ced329f8
Maturus_Lux <43556620+MaturusLux@users.noreply.github.com>
2025-11-09 11:55:36 -0300

    Update 06_media_geometrica_vs_aritmetica.py
    Función:
    Media Geometrica: Incremento de Gastos en Reparaciones.
    Data: Datos Manuales.
    Lenguaje: Python 3.7.
    Librerias: numpy, matplotlib.

--- funciones_basicas/06_media_geometrica_vs_aritmetica.py
+++ funciones_basicas/06_media_geometrica_vs_aritmetica.py
@@ -1,29 +1,34 @@
-import sys
+# 06 Medía Aritmetica para determinar el punto de inflexión de incremento de gastos en Maquinaria X, a modo comparativo se determina la Aritmetica en el mismo ejercicio.
 import matplotlib.pyplot as plt
 import numpy as np  # Ya lo tienes
 
 # Ej Orientado a Media Aritmetica
-# Ejercicio 1: crecimiento lineal 10 Unidades, incorporación Cabezas de Vacunos por Año
-#data = [10, 20, 30, 40, 50, 60, 70]
+# Dato 1: Gasto Fijo Anual de Maquinaría ya en uso, gasto en Dolares.
+data1 = [100, 200, 300, 400, 500, 600, 700, 800]
 
-# Ej Orientado a Media Geometrica, 
-# Ekjercicio 2: crecimiento natural del 34/36% de los animales sin vender, sin incorporar
-# Datos: crecimiento natural del ganado (geométrico), y calcular la Media Geometrica
-data = [10, 13, 19, 25, 34, 46, 63]
+# Ej Orientado a Media Geometrica,
+# Dato 2: Gastos Fijo en Maquinaría nueva, Media Geometrica para comparar y determinar punto de inflexión.
+data2 = [100, 130, 190, 250, 340, 460, 630, 810]
 
 # Calcular medias con numpy
-data = np.array(data)
-arithmetic_mean = np.mean(data)
-geometric_mean = np.exp(np.mean(np.log(data)))  # Media geométrica
+data1 = np.array(data1)
+arithmetic_mean = np.mean(data1)
+#geometric_mean = np.exp(np.mean(np.log(data1)))  # Media geométrica
+
+data2 = np.array(data2)
+#arithmetic_mean = np.mean(data2)
+geometric_mean = np.exp(np.mean(np.log(data2)))  # Media geométrica
 
 # Gráfico
 plt.figure(figsize=(8, 6))
-plt.plot(data, 'o', label='Data Points')
-plt.axhline(y=geometric_mean, color='r', linestyle='--', label=f'Geometric Mean: {geometric_mean:.2f}')
-plt.axhline(y=arithmetic_mean, color='g', linestyle=':', label=f'Arithmetic Mean: {arithmetic_mean:.2f}')
-plt.title('Data Points and Geometric Mean - Nacimiento Cabezas de Ganado (34/36% Anual)')
+plt.plot(data1, color='grey', label='Reparac. Maquin Usada')    # Línea azul para data1
+plt.plot(data2, color='red', label='Reparac. Maquin Nueva')     # Línea roja para data2
+plt.axhline(y=geometric_mean, color='r', linestyle='--', label=f'Media Geométrica: {geometric_mean:.2f}')
+plt.axhline(y=arithmetic_mean, color='grey', linestyle=':', label=f'Media Aritmética: {arithmetic_mean:.2f}')
+plt.title('Comparativa p/Considerar cambio de unidad (unica Variable: Gt. reparación)')
 plt.xlabel('Index (Año)')
-plt.ylabel('Value (Cabezas de Ganado)')
+plt.ylabel('Value (Dolares x Miles)')
 plt.legend()
 plt.grid(True)
 plt.show()
+
